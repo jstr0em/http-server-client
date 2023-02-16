e16b3b156a238b2f265a417e8c232ad966ad3786
Jonathan Ström <jonathanstroem91@gmail.com>
2023-02-16 21:10:00 +0100

    Refactored the server code

--- Server.cpp
+++ Server.cpp
@@ -4,27 +4,35 @@
 #include "stdlib.h"
 #include "unistd.h"
 #include "string.h"
+#include "sys/select.h"
+#include "errno.h"
+
+#define MAX_CLIENTS 10
+#define BUFFER_SIZE 1024
 
 int main(int argc, char *argv[]) {
-
     int server_fd;
     struct sockaddr_in address;
     int addrlen = sizeof(address);
     int new_socket;
-    
-    int opt{1};
-
+    fd_set readfds;
+    int opt = 1;
 
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
         perror("Create socket failed");
         return 0;
     }
 
-    const int port{8080};
+    const int port = 8080;
 
     address.sin_addr.s_addr = htonl(INADDR_ANY);
     address.sin_family = AF_INET;
     address.sin_port = htons(port);
+
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
+        perror("Set socket options failed");
+        return 0;
+    }
 
     if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
         perror("Bind failed");
@@ -36,25 +44,64 @@ int main(int argc, char *argv[]) {
         return 0;
     }
 
+    int max_sd;
+    int clients[MAX_CLIENTS] = {0};
+
     while (1) {
-        if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0) {
-            perror("Accept failed");
-            return 0;
+        FD_ZERO(&readfds);
+        FD_SET(server_fd, &readfds);
+        max_sd = server_fd;
+
+        for (int i = 0; i < MAX_CLIENTS; i++) {
+            int sd = clients[i];
+            if (sd > 0) {
+                FD_SET(sd, &readfds);
+                if (sd > max_sd) {
+                    max_sd = sd;
+                }
+            }
         }
 
-        char buffer[1024] = { 0 };
-
-        int valread = read(new_socket, buffer, 1024);
-
-        printf("%s\n", buffer);
-        if (valread < 0) {
-            printf("Nothing to read");
+        int activity = select(max_sd + 1, &readfds, NULL, NULL, NULL);
+        if ((activity < 0) && (errno!=EINTR)) {
+            printf("Select error");
         }
 
-        char* greetings = "Hello from the server";
-        write(new_socket, greetings, strlen(greetings));
+        if (FD_ISSET(server_fd, &readfds)) {
+            if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0) {
+                perror("Accept failed");
+                return 0;
+            }
 
-        close(new_socket);
+            printf("New client connected\n");
+
+            for (int i = 0; i < MAX_CLIENTS; i++) {
+                if (clients[i] == 0) {
+                    clients[i] = new_socket;
+                    break;
+                }
+            }
+        }
+
+        for (int i = 0; i < MAX_CLIENTS; i++) {
+            int sd = clients[i];
+            if (FD_ISSET(sd, &readfds)) {
+                char buffer[BUFFER_SIZE] = { 0 };
+
+                int valread = read(sd, buffer, BUFFER_SIZE);
+
+                if (valread == 0) {
+                    printf("Client disconnected\n");
+                    close(sd);
+                    clients[i] = 0;
+                } else {
+                    printf("%s\n", buffer);
+                    char* greetings = "Hello from the server";
+                    write(sd, greetings, strlen(greetings));
+                }
+            }
+        }
     }
+
     return 0;
 }
